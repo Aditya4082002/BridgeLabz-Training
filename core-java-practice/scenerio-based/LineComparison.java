c3c0ccaa228c2bae73d033fef253b8926843dea1
Aditya Singh <adityasingh4082002@gmail.com>
2025-12-30 13:52:35 +0530

    [Aditya Singh] Add. Added UC 2 in LineComparision.

--- core-java-practice/scenerio-based/LineComparison.java
+++ core-java-practice/scenerio-based/LineComparison.java
@@ -6,8 +6,7 @@ class LineComparison{
 		
 		//Start
 		System.out.println("Welcome to Line Comparison Computation Program");
-		
-		//UC 1
+		//User input for Co-ordinates of both lines
 		System.out.println("Enter x1 Co-ordinate of point A : ");
 		int x1 = input.nextInt();
 		System.out.println("Enter y1 Co-ordinate of point A : ");
@@ -17,16 +16,47 @@ class LineComparison{
 		System.out.println("Enter y2 Co-ordinate of point B : ");
 		int y2 = input.nextInt();
 		
-		double length = lengthOfLine(x1,y1,x2,y2);
+		System.out.println("Enter x3 Co-ordinate of point c : ");
+		int x3 = input.nextInt();
+		System.out.println("Enter y3 Co-ordinate of point c : ");
+		int y3 = input.nextInt();
+		System.out.println("Enter x4 Co-ordinate of point D : ");
+		int x4 = input.nextInt();
+		System.out.println("Enter y4 Co-ordinate of point D : ");
+		int y4 = input.nextInt();
 		
-		System.out.println("Length of the line is " + length);
+		//calculate length of both lines
+		Double lengthOfLineOne = lengthOfLine(x1,y1,x2,y2);
+		Double lengthOfLineTwo = lengthOfLine(x3,y3,x4,y4);
+		
+		//display length of both lines
+		System.out.println("Length of the line one is " + lengthOfLineOne);
+		System.out.println("Length of the line two is " + lengthOfLineTwo);
+		
+		//check for equal
+		boolean areEqual = compareLines(lengthOfLineOne,lengthOfLineTwo);
+		
+		if(areEqual){
+			System.out.println("Both lines are equal.");
+		}else{
+			System.out.println("Both lines are not equal.");
+		}
 		
 		
 		//close scanner
 		input.close();
 	}
-	static double lengthOfLine(int x1,int y1,int x2,int y2){
+	//UC 1
+	static Double lengthOfLine(int x1,int y1,int x2,int y2){
 		double length=Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
 		return length;
+	}
+	
+	//UC 2
+	static boolean compareLines(Double lengthOne, Double lengthTwo){
+		if(lengthOne.equals(lengthTwo)){
+			return true;
+		}
+		return false;
 	}
 }

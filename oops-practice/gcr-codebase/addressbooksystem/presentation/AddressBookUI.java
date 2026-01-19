f50e1f6ca36595ee57bc234d3c08d6ead193b42c
Aditya Singh <adityasingh4082002@gmail.com>
2026-01-19 15:20:42 +0530

    [Aditya Singh] Add. Added UC-5, Refactor. renamed controler to presentation.

--- oops-practice/gcr-codebase/addressbooksystem/presentation/AddressBookUI.java
+++ oops-practice/gcr-codebase/addressbooksystem/presentation/AddressBookUI.java
@@ -1,10 +1,57 @@
-package addressbooksystem.controller;
-import addressbooksystem.service.*;
+package addressbooksystem.presentation;
+
 import java.util.Scanner;
 
-public class AddressBookController {
-	ContactService service = new ContactService();
+import addressbooksystem.service.ContactService;
+
+public class AddressBookUI{
 	Scanner input = new Scanner(System.in);
+	ContactService service = new ContactService();
+	public void startApp() {
+		boolean running = true;
+		int choice;
+		System.out.println("-----------------------------");
+		System.out.println("Welcome To AddressBook App.");
+		System.out.println("-----------------------------");
+		while(running) {
+			System.out.println("-----------------------------");
+			System.out.println("Options : ");
+			System.out.println("1 - Add Contact.");
+			System.out.println("2 - Edit Contact.");
+			System.out.println("3 - Delete Contact.");
+			System.out.println("4 - Display All Contacts.");
+			System.out.println("5 - Exit");
+			System.out.println("-----------------------------");
+			
+			System.out.println("Enter your chooice : ");
+			choice  = input.nextInt();
+			input.nextLine();//consume extra line
+			
+			switch(choice) {
+				case 1:
+					addContactFromUser();
+					break;
+				case 2:
+					editContact();
+					break;
+				case 3:
+					deleteContactByName();
+					break;
+				case 4:
+					displayContacts();
+					break;
+				case 5:
+					System.out.println("Thanks for using Address Book App.");
+					return;
+				default:
+					System.out.println("Invalid input.");
+			}
+			
+			if(!running) {
+				break;
+			}
+		}
+	}
 	
 	//Add contacts 
 	public void addContactFromUser() {
@@ -25,8 +72,7 @@ public class AddressBookController {
 		int zip = input.nextInt();
 		System.out.println("Enter phone : ");
 		long phone = input.nextLong();
-		//read extra space
-		input.nextLine();
+		input.nextLine();//consume extra line
 		
 		service.addNewContact(firstName, lastName, address, city, state, email, zip, phone);
 	}
@@ -54,9 +100,7 @@ public class AddressBookController {
 		int zip = input.nextInt();
 		System.out.println("Enter phone : ");
 		long phone = input.nextLong();
-		//read extra space
-		input.nextLine();
-		
+		input.nextLine();//consume extra line
 		
 		service.editExixtingContact(name,firstName, lastName, address, city, state, email, zip, phone);
 	}
@@ -69,8 +113,6 @@ public class AddressBookController {
 		
 	}
 
-	
-	
 	//Display contacts
 	public void displayContacts() {
 		service.displayAllContacts();
